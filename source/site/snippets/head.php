356b2e49463228dd9f2fd3a283d46c8ff131bbd3
philipmarnef <philip@panache.works>
2017-10-17 08:59:25 +0200

    Added head snippet with css & js links

---
+++ source/site/snippets/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+
+  <!-- assets -->
+  <?= css('assets/css/screen.css') ?>
+  <?= js('assets/js/app.js') ?>
+
+</head>
